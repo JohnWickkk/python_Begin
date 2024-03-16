7e34305859b956ad401626f44b02e731c5e0f1d4
Eugene Bondarev <bondarev909@gmail.com>
2024-03-16 14:47:01 +0200

    Home_work_4 and some changes in HomeWork_3

--- Lesson_4/HoMeWork_4_5.py
+++ Lesson_4/HoMeWork_4_5.py
@@ -1,3 +1,12 @@
-# Задача 5.Напишіть програму яка візьме стрінг та повторить її задану кількість раз. /
-# До прикладу ми маємо стрінгу 'abc' та число 3. Результатом роботи програми має бути "abcabcabc"
+# Task 5. Write a program that takes a string and repeats it a given number of times. /
+# For example, we have the string 'abc' and the number 3. The result of the program should be 'abcabcabc'
 
+print('pls input some sequence of symbols')
+Your_str_4_5 = str(input())  # getting inputted sequence"
+print('input  q-ty of multiple')
+multiple = int(input())  # getting multiplier
+if isinstance(multiple, str):
+    print('Your multiple does`t looks like a number')
+else:
+    multiple_sequence = Your_str_4_5 * multiple
+    print(multiple_sequence)
--- Lesson_4/HomeWork_4_6.py
+++ Lesson_4/HomeWork_4_6.py
@@ -1,4 +1,23 @@
-#Задача 6. Написати програму для підрахунку конвертації валюти: UAH --> USD
-#доларів США --> грн
-#UAH --> EUR
-#євро --> грн
+# Task 6. Write a program to convert currencies:
+# UAH --> USD
+# USD --> UAH
+# UAH --> euro
+# euro --> UAH
+
+USD_rate = float(38.50)
+euro_rate = float(42.00)
+UAH_rate = float(1.00)
+print('What currency do you have: USD, UAH, EURO')
+Income_currency = (input())  # getting current currency
+print('What currency do you WANT to HAVE: USD, UAH, EURO')
+Outcome_currency = (input())  # getting wanted currency
+print('What q-ty of', {Income_currency}, 'do you have?')
+Income_amount = (float(input()))  # getting income amount
+
+if Income_currency == 'UAH' and Outcome_currency == 'USD':
+    Outcome_amount = float(Income_amount / USD_rate)
+elif Income_currency == 'UAH' and Outcome_currency == 'euro':
+    Outcome_amount = float(Income_amount / euro_rate)
+elif Income_currency == 'UAH' and Outcome_currency == 'UAH':
+    Outcome_amount = float(Income_amount / UAH_rate)
+print('Amount of', Outcome_currency, 'after converting is', Outcome_amount)
