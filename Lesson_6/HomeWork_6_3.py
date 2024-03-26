01a13fbb83cff583f04818cab9706f5b175251be
Eugene Bondarev <bondarev909@gmail.com>
2024-03-26 17:57:14 +0200

    new lesson 6

--- Lesson_6/HomeWork_6_3.py
+++ Lesson_6/HomeWork_6_3.py
@@ -4,8 +4,8 @@
 #1 2 3
 #1 2 3 4
 #1 2 3 4 5
-for i in range(1, 6):
+for i in range(1, 6):  # making a range of number
   for n in range(1, i + 1):
-    print(n, end=" ")
+    print(n, end=" ")  # modify print function
   print()
 
